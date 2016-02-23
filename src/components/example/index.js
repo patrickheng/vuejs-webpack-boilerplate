b68e8b9d1528fa7a8ec19b26f23beab2e3ad4aeb
Patrick HENG <hengpatrick.pro@gmail.com>
2016-02-23 22:53:50 +0100

    Use es6 syntax for method declaration

---
+++ src/components/example/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+import {
+  WINDOW_RESIZE
+} from '../../config/messages';
+
+export default Vue.extend({
+
+  template: require('./template.html'),
+
+  data: function() {
+
+    return {
+      _hidden: null
+    };
+  },
+
+  created: function() {
+    this.bind();
+  },
+
+  ready: function() {
+
+    this.addEventListeners();
+  },
+
+  beforeDestroy: function() {
+
+    this.removeEventListeners();
+  },
+
+  methods: {
+
+    /*
+     * Binding & Events
+     */
+
+    bind: function() {
+    },
+
+    addEventListeners: function() {
+      this.$on(WINDOW_RESIZE, this.onWindowResize);
+    },
+
+    removeEventListeners: function() {
+      this.$off(WINDOW_RESIZE, this.onWindowResize);
+    },
+
+    onWindowResize: function(width, height) {
+    }
+
+  },
+
+  transitions: {
+
+  },
+
+  components: {}
+});
