b849af7f4d3129008f988f13b2a263d393c77e46
Patrick Heng <hengpatrick.pro@gmail.com>
2016-06-28 18:51:15 +0200

    Server - Use express static to serve static folder

--- server.js
+++ server.js
@@ -1,42 +1,52 @@
 /*eslint-env node*/
-import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
 import history from 'connect-history-api-fallback';
+import portNumber from 'port-number';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack/webpack.dev.config.babel.js';
+import devConfig from './webpack/webpack.dev.config.babel.js';
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
-const ip = 'localhost';
+const env = process.env.NODE_ENV;
+const isDeveloping = env !== 'production';
+const port = process.env.PORT ? process.env.PORT : portNumber();
+const ip = isDeveloping ? '0.0.0.0' : '0.0.0.0';
 
 const app = express();
-const compiler = webpack(config);
 
-app.use(history());
+app.use( history() );
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  headers: {'Access-Control-Allow-Origin': '*'},
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false
-  }
-}));
+if( isDeveloping ) {
 
-app.use(webpackHotMiddleware(compiler));
+  const compiler = webpack( devConfig );
 
-app.get('*', (req, res) => res.sendFile(path.join( __dirname, 'index.html')));
+  app.use( webpackDevMiddleware( compiler, {
+    publicPath: devConfig.output.publicPath,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  }) );
+
+  app.use( webpackHotMiddleware( compiler ) );
+
+  // Serve pure static assets
+  app.use( express.static( './static' ) );
+
+} else {
+
+  app.use( express.static( __dirname + '/dist' ) );
+}
 
 app.listen( port, ip, error => {
-  if (error) throw error;
+  if ( error ) throw error;
 
   /*eslint-disable no-console */
-  console.info(`=> 🚧 Listening on port ${port}. Open up http://${ip}:${port}/ in your browser.`);
+  console.info( `[${env}] Listening on port ${port}. Open up http://${ip}:${port}/ in your browser.` );
   /*eslint-enable no-console */
 });
