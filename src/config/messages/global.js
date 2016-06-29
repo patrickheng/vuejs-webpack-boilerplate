21ee97264dba320edbe76812025caafa8fedffba
Patrick Heng <hengpatrick.pro@gmail.com>
2016-06-29 10:38:28 +0200

    Messages - Separate in folders and merge

---
+++ src/config/messages/global.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const globalMessages = {
+
+  /*
+   * WINDOW
+   */
+
+  WINDOW_RESIZE: 'WINDOW_RESIZE',
+  WINDOW_ON_FOCUS: 'WINDOW_ON_FOCUS',
+  WINDOW_ON_BLUR: 'WINDOW_ON_BLUR',
+
+  /*
+   * RESOURCES
+   */
+  RESOURCES_READY: 'RESOURCES_READY'
+
+};
+
+module.exports = globalMessages;
---
+++ src/config/messages/index.js
@@ -0,0 +1,7 @@
+import global from './global';
+
+const messages = {
+  ...global
+};
+
+module.exports = messages;
